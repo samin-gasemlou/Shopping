48a8b97269290ca2118891f049bc0c8d0b193ed9
samin <samingasemlou@gmail.com>
2022-08-02 13:23:47 +0430

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,14 @@
+const bar = document.querySelector('.fa-bars');
+const nav= document.querySelector('#navbar');
+const close=document.querySelector('#close')
+
+if (bar) {
+    bar.addEventListener('click', function(){
+        nav.classList.add('active');
+    });
+};
+if (close) {
+    close.addEventListener('click', function(){
+        nav.classList.remove('active');
+    })
+}
